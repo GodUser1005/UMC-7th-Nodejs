640f6e8bba35a7ae4d5228952fbe480a744d10ae
GodUser1005 <jijone1005@naver.com>
2024-11-06 16:05:41 +0900

    환경 변수 설정

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
 //const express = require('express') // -> CommonJS
-import express from 'express'        // -> ES Module
+import dotenv from "dotenv";
+import express from 'express';        // -> ES Module
+import cors from 'cors';
+
+dotenv.config();  // .env 파일로 부터 환경변수를 읽고 이를 process.env 라는 객체를 통해 접근 허용
 
 const app = express()
-const port = 3000
+const port = process.env.PORT;
+
+app.use(cors());  // cors 방식 허용?
+app.use(express.static('public'));  // 정적 파일 접근?
+app.use(express.json());  // request body를 json으로 해석할 수 있도록 함(JSON형태의 요청 body를 파싱)
+app.use(express.urlencoded({extended: false})); // 단순 객체 문자열 형태로 본문 데이터 해석
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
