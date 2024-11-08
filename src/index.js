c575cfa71f1068feb40f63aebc1d2d0567212e6d
GodUser1005 <jijone1005@naver.com>
2024-11-08 16:47:57 +0900

    DB 환경설정 및 pool 생성

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 //const express = require('express') // -> CommonJS
 import dotenv from "dotenv";
-import express from 'express';        // -> ES Module
-import cors from 'cors';
+import express from "express";        // -> ES Module
+import cors from "cors";
 
 dotenv.config();  // .env 파일로 부터 환경변수를 읽고 이를 process.env 라는 객체를 통해 접근 허용
 
@@ -14,10 +14,10 @@ app.use(express.json());  // request body를 json으로 해석할 수 있도록 
 app.use(express.urlencoded({extended: false})); // 단순 객체 문자열 형태로 본문 데이터 해석
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+  res.send("Hello World!");
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
 
